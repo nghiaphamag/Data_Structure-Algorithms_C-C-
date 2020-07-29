322b78cf6913bbcbd1e9e126c1c0ce686a8edb73
Nghia Pham <nghiaphamsg@gmail.com>
2020-07-29 22:07:49 +0900

    Fin logic

--- Merge/main.c
+++ Merge/main.c
@@ -21,11 +21,11 @@ struct Array* Merge(struct Array *arr1, struct Array *arr2) {
             arr3->A[k++] = arr2->A[j++];
     }
     for (;i < arr1->length; i++)
-        arr3->A[k++] = arr1->A[i++];
+        arr3->A[k++] = arr1->A[i];
     for (;j < arr2->length; j++)
-        arr3->A[k++] = arr2->A[j++];
+        arr3->A[k++] = arr2->A[j];
     arr3->length = arr1->length + arr2->length;
-    arr3->size = 10;
+    arr3->size = 12;
 
     return arr3;
 }
